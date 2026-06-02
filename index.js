31f5ca560ce1164234fb969c2d5441fb5853a17f
tushargangarde28-eng <tushargangarde28@gmail.com>
2026-06-02 20:05:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', function (req, res) {
   res.render("index.ejs");
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
